93f457a3466a912f4f879484acb7e870dc5d01d7
ander <anderdefector@gmail.com>
2020-12-19 09:19:56 -0600

    Cambios

---
+++ configuracion.bash
@@ -0,0 +1,13 @@
+echo "Revision de sistema"
+
+sudo apt update
+sudo apt upgrade
+
+echo "Instalacion boost"
+
+sudo apt-get install libboost-all-dev
+
+echo "Instalacion Eigen"
+
+sudo apt-get install libeigen3-dev
+
